c6a38b737fa087edff683ff1d6de45f59370a07e
lichanghang <lichanghang@meituan.com>
2019-04-10 20:25:17 +0800

    xxx

--- dev_client_container.sh
+++ dev_client_container.sh
@@ -7,3 +7,4 @@ cur_path=$(cd `dirname $0`;pwd)
 # docker run --name ept_client -v $cur_path/client:/app/client -d -p 8080:8080 -t ept_client:v0.1 http-server /dist
 docker run --name ept_client_dev -v $cur_path/client:/app/client -d -p 8080:8080 -t ept_client:v0.1 npm run serve
 
+#docker logs -f -t --tail 100 ept_client_dev
